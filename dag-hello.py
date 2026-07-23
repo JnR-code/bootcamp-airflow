54a465cd50ab444b235dfa802b286e1ffebc5edf
Julien Geniteau <ext.julien.geniteau@sncf.fr>
2026-07-23 14:15:54 +0200

    Enchaineemnt

--- dag-hello.py
+++ dag-hello.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.sensors.time_delta import TimeDeltaSensor
-from datetime import timedelta
+from airflow.utils.trigger_rule import TriggerRule
  
 with DAG(
     dag_id="dag_formation",
@@ -15,20 +14,23 @@ with DAG(
         task_id="hello",
         bash_command="echo 'Airflow est prêt sur cette EC2'; hostname; date"
     )
-    
+
     hello2 = BashOperator(
         task_id="hello2",
-        bash_command="echo 'Hello 2'"
+        bash_command="echo 'Hello 2 - simulation erreur'; exit 1"  # force l'échec
     )
-    hello3= BashOperator(
+
+    hello3 = BashOperator(
         task_id="hello3",
-        bash_command="echo 'Hello 3'"
+        bash_command="echo 'Hello 3'",
+        trigger_rule=TriggerRule.ALL_SUCCESS  # s'exécute seulement si hello a réussi (c'est le défaut, explicite ici pour la clarté)
     )
-    
-    wait = TimeDeltaSensor(
-    task_id="attendre_10s",
-    delta=timedelta(seconds=10),
-    mode="reschedule"  # libère le slot pendant l'attente
-)
-    
-    hello >> wait >> [hello2, hello3]
+
+    hello4 = BashOperator(
+        task_id="hello4",
+        bash_command="echo 'Hello 4'",
+        trigger_rule=TriggerRule.ALL_FAILED  # s'exécute seulement si hello2 a échoué
+    )
+
+    hello >> hello3
+    hello2 >> hello4
